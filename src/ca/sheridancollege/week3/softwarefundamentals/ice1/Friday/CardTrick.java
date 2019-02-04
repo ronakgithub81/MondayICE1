cb2c371542abe78afac2430aa61097618f6dfca8
KaroonaB <Karoonabhagat1999@gmail.com>
2019-02-04 14:00:43 -0500

    commiting object added file

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,14 +1,15 @@
-
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Karoona Bhagat
+ * Student Number: 991468639
  */
-public class CardTrick {
-    public static void main(String[] args)
-    {
-       
-    }
-    
+public class CardTrick
+{
+   public static void main (String[] args)
+   {
+      Card karoona = new Card();
+   }
+
 }
