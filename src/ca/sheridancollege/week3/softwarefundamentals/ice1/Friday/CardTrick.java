d2bfa318fc5b0ea6a6a4aba24455242bc8eb5848
John <chejiaha@sheridancollege.ca>
2019-02-04 14:09:14 -0500

    adding modifiers and card class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,17 +8,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       //object of card class
-        Card cardObj = new Card();
-        
-        //setting suit and value
-        cardObj.setSuit("Hearts");
-        cardObj.setValue(1);
-        
-        //printing suit and value
-        System.out.println(cardObj.getSuit());
-        System.out.println(cardObj.getValue());
-        //add
+       
         
     }
     
