84055070509ef5313dfa8db41658eb1f8b23b482
tranaa <tranaa@sheridancollege.ca>
2019-02-04 14:14:31 -0500

    Added another card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -10,7 +10,7 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card1 = new Card();
-       Card card1 = new Card();
+       Card card2 = new Card();
        card1.setSuit(Card.SUITS[0]);
        card1.setValue(2);
        System.out.println("Suit: " + card1.getSuit());
