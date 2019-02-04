f0faa01157763ed317d2ee8beb719db67c784d23
DanMorrison54 <modaniel@sheridancollege.ca>
2019-02-04 13:41:15 -0500

    Daniel Morrison's commit for ICE1

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Daniel morrison
+ * Student num: 991516745
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card=new Card();
     }
     
 }
