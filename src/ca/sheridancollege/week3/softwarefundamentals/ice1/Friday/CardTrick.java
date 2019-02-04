8820a4d4bf22864121bf739b37590c42c4a2952d
Kiu <Kiu@DESKTOP-PQ8AUU4>
2019-02-04 13:53:32 -0500

    create variable dimond Trang Nguyen

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,13 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ * trangnguyen  991531493
  * @author Ronak
  */
 public class CardTrick {
-    public static void main(String[] args)
+    public static void main(String[] args)           
     {
-       
+       int dimond;
     }
     
 }
