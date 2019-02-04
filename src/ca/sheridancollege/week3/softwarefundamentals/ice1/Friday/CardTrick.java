c3beb23de5d5f7e472ee54d4181f3b386db6cac9
DanMorrison54 <46901944+DanMorrison54@users.noreply.github.com>
2019-02-04 13:46:27 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,7 +9,10 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card card=new Card();
+        Card card=new Card();
+       card.setSuit(card.SUITS[2]);
+       card.setValue(3);
+       System.out.println(card.getValue()+" of "+card.getSuit());
     }
     
 }
