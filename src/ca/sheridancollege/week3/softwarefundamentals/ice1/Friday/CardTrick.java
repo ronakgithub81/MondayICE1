d7efc6eab56c89828d1a7c16ea2a59695047c3bb
Andrew Williams <46901721+SheridanStudent@users.noreply.github.com>
2019-02-04 14:24:01 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -12,7 +12,7 @@ public class CardTrick {
     {
        Card C1 = new Card();
        C1.setSuit("Hearts");
-       C1.SetValue(8);
-       System.out.println("The card was a " + C1.getValue() + " of " + C1.getSuits() + ".");
+       C1.setValue(8);
+       System.out.println("The card was a " + C1.getValue() + " of " + C1.getSuit() + ".");
     }
 }
