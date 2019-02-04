cdf77b27023bda5803e5e49ee82e56fa5ed35a8d
Joey <camph@sheridancollege.ca>
2019-02-04 14:02:29 -0500

    Adding comments

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,7 +3,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author PHUONG CAM 991529196
  */
 public class CardTrick {
     public static void main(String[] args)
