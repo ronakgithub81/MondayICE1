7ef7cff289eb951e59dcbef34df125912dfc30b8
tranaa <tranaa@sheridancollege.ca>
2019-02-04 14:01:14 -0500

    Added name modifier and student number

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Aaron Tran 
+ * 991525227
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card card1 = new Card();
     }
     
 }
