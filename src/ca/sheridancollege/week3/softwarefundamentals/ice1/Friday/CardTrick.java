fdee4d01c8609720485b9ad20e63b9dca89bd819
ziiiz <omarazi@sheridancollege.ca>
2019-02-04 14:06:13 -0500

    ....

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -6,7 +6,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  * @author Ronak
  */
 public class CardTrick {
-    public static void main(String[] args)
+    public static void   main(String[] args)
     {
        
     }
