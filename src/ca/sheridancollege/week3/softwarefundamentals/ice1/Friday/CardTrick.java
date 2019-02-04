0c9c1c0aa6878ee6e4967e8e57e45fd946655d67
Leah Kim <jeah@sheridancollege.ca>
2019-02-04 13:55:05 -0500

    Commit Success!

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,11 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author JE AH(Leah) Kim  (Student# 991509706)
  */
 public class CardTrick {
     public static void main(String[] args)
     {
+       Card leah = new Card();
        
     }
     
