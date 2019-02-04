f81cb79ee3c13eeab4279e5ada8ce512e2e7ced1
John <chejiaha@sheridancollege.ca>
2019-02-04 13:48:54 -0500

    adding modifiers and card class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,14 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
- * @author Ronak
+ *991530024
+ * @author Jiahao Chen
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       //object of card class
+        Card cardObj = new Card();
     }
     
 }
