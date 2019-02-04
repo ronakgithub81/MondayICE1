76940337028ef4b8cb3414e5d50f4a123996dcfb
pellemas <46901800+pellemas@users.noreply.github.com>
2019-02-04 13:52:05 -0500

    Added print line for the card
    Added print line for the card

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,8 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card = new Card("Hearts", 4);
+        
+        System.out.println("The suit is " + card.getValue() + " and the value is " + card.getSuit());
     }
     
 }
