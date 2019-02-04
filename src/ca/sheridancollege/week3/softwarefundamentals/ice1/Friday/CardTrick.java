337ce07f0ac2e5ea617c846e34621d729728b56c
JessySinghPawar <pawarjes@sheridancollege.ca>
2019-02-04 14:13:14 -0500

    Created Object of Card class in CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,13 +1,15 @@
-
+//Student: Jessy Singh Pawar
+//Student ID: 991524234
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Jessy
  */
 public class CardTrick {
     public static void main(String[] args)
     {
+            Card jessy = new Card();
        
     }
     
