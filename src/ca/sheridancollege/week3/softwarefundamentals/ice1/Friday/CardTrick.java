c0d5ee19c1c82d79396a900b61c02ef618282212
Beattija <46901896+Beattija@users.noreply.github.com>
2019-02-04 14:12:27 -0500

    Added and print out suit + number

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -12,6 +12,10 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card newCard = new Card();
+       newCard.setSuit("Spades");
+       newCard.setValue(5);
+       System.out.println(newCard.getSuit());
+       System.out.println(newCard.getValue());
     }
     
 }
