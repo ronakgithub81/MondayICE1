4a415fcda9f32cad6dd4952c9a113c9b9afa0292
Andrew Williams <SheridanStudent@users.noreply.github.com>
2019-02-04 14:48:13 -0500

    inclass exercise

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -14,5 +14,10 @@ public class CardTrick {
        C1.setSuit("Hearts");
        C1.setValue(8);
        System.out.println("The card was a " + C1.getValue() + " of " + C1.getSuit() + ".");
+       
+       Card C2 = new Card();
+       C2.setSuit("Hearts");
+       C2.setValue(4);
+       System.out.println("The card was a " + C2.getValue() + " of " + C2.getSuit() + ".");
     }
 }
