533204b7d74e7041d8d5a7d873f7203cb977be3c
Abhinav <kheraab@sheridancollege.ca>
2019-02-04 14:24:19 -0500

    Creating a new object

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Abhinav Khera
+ * Student #: 991527005
  */
 public class CardTrick {
-    public static void main(String[] args)
-    {
-       
+    public static void main(String[] args){
+              
+        Card card = new Card();
     }
     
 }
