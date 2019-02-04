8d19091d0567e0b9dab86b67e2c3fccbfbc7fd46
Anthony <dossan@sheridan.ca>
2019-02-04 13:47:26 -0500

    step3

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,14 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author AnthonyDoss
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card();
+
+
     }
     
 }
