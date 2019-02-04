5c7c2ddad4fbf798ab3987f9a40da6c0be52479d
tranaa <46901859+tranaa@users.noreply.github.com>
2019-02-04 14:10:01 -0500

    Made a card, set value and suit, and printed it

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -10,6 +10,11 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card card1 = new Card();
+       Card card1 = new Card();
+       card1.setSuit(Card.SUITS[0]);
+       card1.setValue(2);
+       System.out.println("Suit: " + card1.getSuit());
+       System.out.println("Value: " + card1.getValue());
     }
     
 }
