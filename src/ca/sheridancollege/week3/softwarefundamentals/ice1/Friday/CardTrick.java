74fb60aaa9300d0282cc7e579bdcc4ee33ae2bbf
ziiiz <omarazi@sheridancollege.ca>
2019-02-04 14:34:51 -0500

    ....

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,14 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ *991531104
  * @Aziz Omar
  */
 public class CardTrick {
     public static void   main(String[] args)
     {
        Card card = new Card();
+        Card card2 = new Card();
             card.setSuit("Hearts");
                 card.setValue(3);
         System.out.println(card.getValue());
