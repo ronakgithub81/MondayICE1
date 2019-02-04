256ba9671f0e13d03c4fc3bd69f0933a1cee58f3
maiph0907 <maiph@shernet.sheridancollege.ca>
2019-02-04 13:58:26 -0500

    adding my name as modifier and student number

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -2,13 +2,14 @@
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
- *
+ * Student Number: 991529666
+ * Modifier: Phuong Mai
  * @author Ronak
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card newCard = new Card();
     }
     
 }
