afd8524094b46eb05f699fd04cd1441da2135ff2
violantf <46901932+violantf@users.noreply.github.com>
2019-02-04 14:01:13 -0500

    added print card values

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,10 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args) {
        Card violantf = new Card();
+        violantf.setSuit("Hearts");
+        violantf.setValue(7);
+        
+        System.out.println("Suit: " + violantf.getSuit + "\nVlaue: " + violantf.getVlaue);
     }
     
 }
