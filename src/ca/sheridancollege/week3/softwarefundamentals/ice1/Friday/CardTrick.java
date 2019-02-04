79371e8e48b2088ba5955041ae6a0f4d9c43a413
ormondg <46901909+ormondg@users.noreply.github.com>
2019-02-04 14:03:13 -0500

    Card value set and printed
    created a 7 of hearts and printed it

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,7 +8,11 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card cards = new Card(); // object created
+       Card card = new Card(); // object created
+       card.setSuit("Hearts");
+       card.setValue(7);
+        
+       System.out.println("Card: " + card.getSuit() + " Value: " + card.getValue);
     }
     
 }
