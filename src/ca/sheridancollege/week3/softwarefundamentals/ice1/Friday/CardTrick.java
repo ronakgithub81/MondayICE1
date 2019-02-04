43496c499a50e5f76ea89600c56aa24d7d483834
Kiu <Kiu@DESKTOP-PQ8AUU4>
2019-02-04 14:10:14 -0500

    create and object

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -8,7 +8,8 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)           
     {
-       int dimond;
+       Card trangCard = new Card();
+       
     }
     
 }
