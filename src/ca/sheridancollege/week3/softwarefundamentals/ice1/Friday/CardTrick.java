74608cb730b27cbee33612d1661f709feca37903
Sina <malekikh@sheridancollege.ca>
2019-02-04 13:59:59 -0500

    Software Design ICE1 - Sina Maleki-Kheymehsari

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -1,14 +1,21 @@
-
 package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Sina Maleki-Kheymehsari
+ * Student #: 991512857
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+        Card myCard = new Card();
+        for (int i = 0; i < myCard.SUITS.length; i++) {
+            System.out.print(myCard.SUITS[i] + " ");
+        }
+        myCard.setSuit("Spades");
+        myCard.setValue((int)(Math.random()*(13 - 1)+1));
+        System.out.print("\nPick a card: "+myCard.getSuit());
+        System.out.print("\nCard value: "+myCard.getValue());
     }
     
 }
