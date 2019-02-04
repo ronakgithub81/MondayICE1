6d683aa9defd68d9046363fdadd2f4b0a0155dec
gaviolak <47331552+gaviolak@users.noreply.github.com>
2019-02-04 15:01:42 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -4,10 +4,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 /**
  *
  * @author Ronak
+ * @modifier Khyan Gaviola 991515840
  */
 public class CardTrick {
     public static void main(String[] args)
     {
+       Card card1 = new Card();
+       System.out.printlin(card);
        
     }
     
