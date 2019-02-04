3d8c1e146480928d3c64a26dd2509fb10610d3e9
Andrew Williams <SheridanStudent@users.noreply.github.com>
2019-02-04 14:01:02 -0500

    inclass exercise

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -5,10 +5,11 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  *
  * @author Ronak
  */
+//Student Name: Andrew Williams
+//Student #:991526881
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card C1 = new Card();
     }
-    
 }
