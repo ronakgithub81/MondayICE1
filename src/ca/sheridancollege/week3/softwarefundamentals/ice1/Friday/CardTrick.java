2e637116df3273d38b6092333c55b8f2c58899b1
eleung86@sheridancollege.ca <>
2019-02-04 22:32:49 -0500

    Create second object of Card class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,6 +9,7 @@ public class CardTrick {
     public static void main(String[] args)
     {
        Card c1 = new Card();
+       Card c2 = new Card();
        System.out.println(c1.getValue());
        System.out.println(c1.getSuit());
     }
