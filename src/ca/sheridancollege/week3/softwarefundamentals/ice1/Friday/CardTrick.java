3b00f06914285cece3c9c290a1074c0dc2392b8c
eleung86@sheridancollege.ca <>
2019-02-04 22:25:12 -0500

    Create an object of Card class

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @Ronak
+ * @Edmund
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card();
     }
     
 }
