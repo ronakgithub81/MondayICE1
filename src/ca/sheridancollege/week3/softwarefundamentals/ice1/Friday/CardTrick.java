f9b412348f8e83eb8ac3e05c0c7b664fa5753b0c
eleung86 <46901862+eleung86@users.noreply.github.com>
2019-02-04 21:59:56 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Edmund Leung 991531421
+ * @Ronak
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card c1 = new Card();
+       
     }
     
 }
