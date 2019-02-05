4e136b60c47f4d3fbf30b970e82c7bc6400231c1
eleung86@sheridancollege.ca <>
2019-02-04 19:03:04 -0500

    Step 3

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author Edmund Leung 991531421
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c1 = new Card();
     }
     
 }
