7f70e83c0a43aa36eaa32a6ce1bad08f8f8490a7
synsheridan <46902055+synsheridan@users.noreply.github.com>
2019-02-10 13:38:31 -0500

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -15,7 +15,10 @@ public class Card {
    private String suit; //clubs, spades, diamonds, hearts
    private int value;//1-13
    
-   public Card(){}
+   public Card(String suit, int value){
+      this.suit = suit;
+      this.value = value;
+   }
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
