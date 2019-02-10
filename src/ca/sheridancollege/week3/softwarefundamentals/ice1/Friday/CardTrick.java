a6a35694aa97329e1a7e9fcd605be722ac652129
synsheridan <46902055+synsheridan@users.noreply.github.com>
2019-02-10 13:56:32 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -9,7 +9,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 public class CardTrick {
     public static void main(String[] args)
     {
-       Card card1 = new Card(SUITS[0], 1);
+       Card card1 = new Card(Card.SUITS[0], 1);
         card1.toString();
     }
     
